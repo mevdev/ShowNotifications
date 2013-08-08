7ca30ce02c0c277b51d82689973d15f847aec85e
mevdev <robertlinnemann@gmail.com>
2013-08-08 11:10:52 -0700

    Initial Commit

---
+++ ShowNotifications/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShowNotifications
+//
+//  Created by Finity Design 2 on 8/8/13.
+//  Copyright (c) 2013 Robert Linnemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShowNotifications/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShowNotifications
+//
+//  Created by Finity Design 2 on 8/8/13.
+//  Copyright (c) 2013 Robert Linnemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
